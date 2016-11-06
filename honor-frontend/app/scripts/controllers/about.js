38af08c6eaf218f4d6da73a6cce52472ce036721
Kung Lao <nick.fossilder@gmail.com>
2016-11-06 20:46:04 +0200

    Initial commit. God bless our project!

---
+++ honor-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBoilerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBoilerApp
+ */
+angular.module('angularBoilerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ honor-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBoilerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBoilerApp
+ */
+angular.module('angularBoilerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
