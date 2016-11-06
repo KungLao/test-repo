b2d1f1226a39a680b3e835d921fb51719e4556a2
Kung Lao <num.lock.backspacer@gmail.com>
2016-11-06 23:55:45 +0200

    Added first test battle page

--- honor-frontend/app/scripts/controllers/about.js
+++ honor-frontend/app/scripts/controllers/about.js
@@ -1,17 +1,7 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularBoilerApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the angularBoilerApp
- */
-angular.module('angularBoilerApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('angularBoilerApp').controller('AboutCtrl',[ '$scope', function($scope) {
+
+    $scope.about1 = 'Made by Kung Lao!';
+
+} ]);
--- honor-frontend/app/scripts/controllers/main.js
+++ honor-frontend/app/scripts/controllers/main.js
@@ -1,17 +1,33 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularBoilerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angularBoilerApp
- */
-angular.module('angularBoilerApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('angularBoilerApp').controller('MainCtrl', [ '$scope', 'Character', function($scope, character) {
+
+    reset();
+    
+    $scope.reset = reset;
+    $scope.cast = function(spellName){
+        // TODO: will be replaced with factory
+        if(spellName === 'attack'){
+            console.log('Attack! ' + $scope.testPlayer.getAttackPower());
+            var result = $scope.testPlayer.getAttackPower() * (1 - $scope.bossDummy.getArmor());
+            $scope.bossDummy.damageTaken += result;
+            if($scope.bossDummy.getHp() <= 0){
+                $scope.bossIsDead = true;
+            }
+        }
+    };
+    
+    function reset(){
+        $scope.bossIsDead = false;
+        $scope.testPlayer = character({
+            name : 'Johanna',
+            className: 'tank1'
+        });
+        
+        $scope.bossDummy = character({
+            name : 'Illidan',
+            className: 'boss1'
+        });
+    }
+
+} ]);
